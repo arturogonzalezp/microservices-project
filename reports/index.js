357e50aef446186dd05faed50ac9493118a314d5
Gabriel Trujillo Gómez <gabotrugomez@Gabriels-iMac.local>
2018-04-16 21:29:03 -0500

    Added methods signatures

--- reports/index.js
+++ reports/index.js
@@ -6,6 +6,28 @@ var bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+app.get('/reports/tasks', (req, res) => {
+    let real_time = req.query.real_time;
+
+    // TODO: if real_time is true, then do a query to DB, else get cached results from Reports DB
+});
+
+app.get('/reports/habits', (req, res) => {
+    let real_time = req.query.real_time;
+
+    // TODO: if real_time is true, then do a query to DB, else get cached results from Reports DB
+});
+
+app.get('/reports/tasks/user', (req, res) => {
+    let userId = req.query.userId;
+    // TODO: get results from a given user from tasks DB
+});
+
+app.get('/reports/habits/user', (req, res) => {
+    let userId = req.query.userId;
+    // TODO: get results from a given user from habits DB
+});
+
 app.get('*', (req, res) => {
     res.send('Reports MicroService v1.0.0');
 });
