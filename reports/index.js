aa906e88541d8515d27390635f2c6fb72687d520
Gabriel Trujillo Gómez <gabotrugomez@Gabriels-iMac.local>
2018-04-24 00:21:59 -0500

    Added functionality to all endpoints

--- reports/index.js
+++ reports/index.js
@@ -1,3 +1,4 @@
+
 var express = require('express');
 var app = express();
 var bodyParser = require('body-parser');
@@ -15,6 +16,7 @@ var taskPool = mysql.createPool({
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 
+// Done.
 app.get('/reports/tasks', (req, res) => {
     let real_time = req.query.real_time;
     let todaysDate = Date();
@@ -52,6 +54,7 @@ app.get('/reports/tasks', (req, res) => {
     });
 });
 
+// Done.
 app.get('/reports/habits', (req, res) => {
     let real_time = req.query.real_time;
     let todaysDate = Date();
@@ -88,14 +91,101 @@ app.get('/reports/habits', (req, res) => {
     });
 });
 
+// Done.
 app.get('/reports/tasks/user', (req, res) => {
     let userId = req.query.userId;
-    // TODO: get results from a given user from tasks DB
+    let todaysDate = Date();
+
+    taskPool.getConnection(function (err, conn) {
+        var delayedTasksQuery = "SELECT title FROM Task WHERE due_date < ? AND completed = 0";
+
+        conn.query(delayedTasksQuery, todaysDate, function(delayedTasksErr, delayedTasksResult, fields) {
+            if (delayedTasksErr) throw delayedTasksErr;
+
+            var resultJson = {};
+
+            if (delayedTasksErr == null) {
+                var todaysTasksQuery = "SELECT title FROM Task WHERE due_date = ? AND completed = 0";
+                
+                conn.query(todaysTasksQuery, todaysDate, function(todaysTasksErr, todaysTasksResult, fields2) {
+                    if (todaysTasksErr) throw todaysTasksErr;
+                
+                    if (todaysTasksErr == null) {
+                        resultJson = {
+                            code: 200,
+                            message: "Report successfully created.",
+                            data: {
+                                todaysTasks: todaysTasksResult,
+                                delayedTasks: delayedTasksResult
+                            }
+                        }
+                    }
+
+                    res.send(resultJson);
+                    conn.release();
+                });
+            }
+            else {
+                resultJson = {
+                    code: 400,
+                    message: "Unable to create Report."
+                }
+                res.send(resultJson);
+                conn.release();
+            }
+        });
+    });
 });
 
+// Done.
 app.get('/reports/habits/user', (req, res) => {
     let userId = req.query.userId;
-    // TODO: get results from a given user from habits DB
+
+    let todaysDate = Date();
+
+    taskPool.getConnection(function (err, conn) {
+        var goodHabitsQuery = "SELECT title FROM Habit WHERE score > 50";
+
+        conn.query(goodHabitsQuery, todaysDate, function(goodHabitsErr, goodHabitsResult, fields) {
+            if (goodHabitsErr) throw goodHabitsErr;
+
+            var resultJson = {};
+
+            if (goodHabitsErr == null) {
+                var badHabitsQuery = "SELECT title FROM Habit WHERE score < 0";
+                
+                conn.query(badHabitsQuery, todaysDate, function(badHabitsErr, badHabitsResult, fields2) {
+                    if (badHabitsErr) throw badHabitsErr;
+                
+                    if (badHabitsErr == null) {
+                        resultJson = {
+                            code: 200,
+                            message: "Report successfully created.",
+                            data: {
+                                goodHabits: goodHabitsResult,
+                                badHabits: badHabitsResult
+                            }
+                        }
+                    }
+
+                    res.send(resultJson);
+                    conn.release();
+                });
+            }
+            else {
+                resultJson = {
+                    code: 400,
+                    message: "Unable to create Report."
+                }
+                res.send(resultJson);
+                conn.release();
+            }
+        });
+    });
+});
+
+app.get('/discover', (req,res) => {
+    res.send('Reports');
 });
 
 app.get('*', (req, res) => {
